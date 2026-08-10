7c86f4e68666506bb34b2d54c5e37495f28a11bd
anuruddh123 <anuruddhtiwari2022@gmail.com>
2026-08-10 21:16:50 +0530

    fix url

--- src/index.js
+++ src/index.js
@@ -23,7 +23,7 @@ app.use(cookieParser());
 // ✅ CORS middleware comes before routes!
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://chatappsssss.netlify.app/",
     credentials: true,
   })
 );
